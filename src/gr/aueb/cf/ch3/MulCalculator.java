30b660b943c96c40fe05ea83f7f3acac298404a8
P. Tridima <pepi.tridima@gmail.com>
2024-12-16 20:13:20 +0200

    Multiply created

---
+++ src/gr/aueb/cf/ch3/MulCalculator.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.ch3;
+
+public class MulCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 2;
+        int result = 0;
+
+        result = mul(num1,num2);
+
+        System.out.println("mul:" + result);
+
+    }
+
+    public static int mul(int a, int b){
+        return a * b;
+    }
+}
